45e3b8b01e26a9841383b9c15c21eb5008ef92c1
SotnikovOleksii <sotalexe@gmail.com>
2025-12-11 17:39:42 +0200

    Pract4

--- Pract4/order.base/src/main/java/module-info.java
+++ Pract4/order.base/src/main/java/module-info.java
@@ -1,3 +1,9 @@
 module order.base {
+    requires static lombok;
+    requires javafaker;
+    requires java.sql; // <--- ДОДАЙ ЦЕЙ РЯДОК
 
+    exports products;
+    exports order;
+    exports factory;
 }
--- Pract4/order.storage/src/main/java/module-info.java
+++ Pract4/order.storage/src/main/java/module-info.java
@@ -1,2 +1,4 @@
 module order.storage {
+    requires order.base; // Потрібен доступ до класу Order
+    exports repository;  // Дозволяємо іншим бачити репозиторій
 }
